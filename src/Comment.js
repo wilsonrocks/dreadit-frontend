1a0bf8c42ae4536f658f2fc09ba4826e73f55edc
James Wilson <jwilsoncalderdale@gmail.com>
2018-06-28 23:07:33 +0100

    add fetch request to Comment to get avatars and names

--- src/Comment.js
+++ src/Comment.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import Votes from './Votes';
 import moment from 'moment';
+import {BASE_URL} from './constants';
 
 const onChange = voteDelta => console.log(`Score changed by ${voteDelta}`);
 
 class Comment extends React.Component {
+
+    state = {
+        avatarURL: "http://s3.amazonaws.com/hiphopdx-production/2016/02/Chuck-D_02-11-2016.jpg",
+        name: 'totty',
+    }
+
+    componentDidMount () {
+        const {created_by} = this.props;
+        
+            fetch(`${BASE_URL}/users/${created_by}`)
+            .then(response => response.json())
+            .then(({user:{name, avatar_url}}) => {
+                this.setState({name, avatar_url});
+            });
+    }
+
+
     render () {
-        const {timestamp, votes, avatarURL, name, body, id} = this.props;
+        const {created_at, votes, body, id} = this.props;
+        const {avatar_url, name} = this.state;
         return (
             <div className="media">
 
-                <div className="media-left">
+                <div className="user media-left has-text-centered">
                     <figure className="image is-64x64">
-                        <img src={avatarURL} alt={name}/>
+                        <img src={avatar_url} alt={name}/>
                     </figure>
                     <p>{name}</p>
-                    <p>{moment(timestamp).fromNow()}</p>
+                    <p>{moment(created_at).fromNow()}</p>
                 </div>
 
                 <div className="media-content">
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://immense-spire-53107.herokuapp.com/api';
