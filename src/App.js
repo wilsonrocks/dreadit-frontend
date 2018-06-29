3770ca8cf417c46eb85765f65882d75080ddc725
James Wilson <jwilsoncalderdale@gmail.com>
2018-06-29 16:48:38 +0100

    add navbar to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import Article from './Article';
+import logo from './assets/drawing.svg'
 import "bulma/css/bulma.css";
 import "./news.css";
 
+
 function App () {
-    return <Article _id="5b340bb9f0ac620014eca86f"/>
+    return (
+
+    <div className="app">
+
+      <section >
+      
+      <nav className="navbar">
+      
+      <div className="navbar-brand logo-with-words">  
+        <img src={logo} className="logo"/>
+        <span className="title dreadit">DREADIT</span>
+      </div>
+
+        <div className="level-item">
+
+            <a className="navbar-item">Latest</a>
+            <a className="navbar-item">Hottest</a>
+            <a className="navbar-item">Authors</a>
+            </div>
+          </nav>
+
+      </section>
+      <Article _id="5b340bb9f0ac620014eca86f"/>
+    </div>
+    );
   };
 
 export default App;
