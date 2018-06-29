427f84cbd55bbafa91f37ee06d344f6bf596f600
James Wilson <jwilsoncalderdale@gmail.com>
2018-06-29 10:39:07 +0100

    add helper function to get topic name from ID and use it in Article

--- src/Article.js
+++ src/Article.js
@@ -2,20 +2,32 @@ import React from 'react';
 import Avatar from './Avatar';
 import CommentList from './CommentList';
 import {BASE_URL} from './constants';
+import {getDetailsFromUserId, getDetailsFromTopicId} from './helpers';
 
 class Article extends React.Component {
     
     state = {
-        name: 'Rainbow Dash',
+        name: null,
         topic: 'cider',
         avatar_url: 'https://vignette.wikia.nocookie.net/mlpfanart/images/8/8e/Rainbow_Dash_by_Nethear.png/revision/latest?cb=20120530035934',
         comments: [],
     };
 
     componentDidMount () {
-        fetch(`${BASE_URL}/articles/${this.props._id}/comments`)
+        const {_id, created_by, belongs_to} = this.props;
+
+        fetch(`${BASE_URL}/articles/${_id}/comments`)
         .then(response => response.json())
         .then(({comments}) => this.setState({comments}));
+
+        getDetailsFromUserId(created_by)
+        .then(details => this.setState(details));
+
+        getDetailsFromTopicId(belongs_to)
+        .then(details => this.setState(details));
+
+
+
     };
 
     render () {
--- src/helpers.js
+++ src/helpers.js
@@ -4,11 +4,25 @@ export function getDetailsFromUserId (userId) {
     if (localStorage[userId]) return Promise.resolve(JSON.parse(localStorage[userId]));
 
     else {
-        fetch(`${BASE_URL}/users/${userId}`)
+        return fetch(`${BASE_URL}/users/${userId}`)
         .then(response => response.json())
         .then( ({user: {name, avatar_url}}) => {
             localStorage[userId] = JSON.stringify({name, avatar_url});
             return ({name, avatar_url});
+        });
+    }
+}
+
+export function getDetailsFromTopicId (topicId) {
+    if (localStorage[topicId]) return Promise.resolve(JSON.parse(localStorage[topicId]));
+
+    else {
+        return fetch(`${BASE_URL}/topics`)
+        .then(response => response.json())
+        .then( ({topics}) => {
+            const {slug,title} = topics.filter(topic => topic._id === topicId)[0]
+            localStorage[topicId] = JSON.stringify({topic: title, slug});
+            return ({topic:title, slug});
         });
     }
 }
