a34110490dbcbbac314a6e8c9dd598c5da052786
James Wilson <jwilsoncalderdale@gmail.com>
2018-07-04 20:13:56 +0100

    add drop down lists on the article page and populate the one for topics

--- src/ArticleList.js
+++ src/ArticleList.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {BASE_URL} from './constants';
 
 import ArticlePreview from './ArticlePreview';
+import OrderDropDown from './OrderDropDown';
 
 
 class ArticleList extends React.Component {
 
     state = {
         articles: [],
+        authors: [],
+        topics: [],
     }
 
     componentDidMount () {
@@ -15,6 +18,15 @@ class ArticleList extends React.Component {
         .then(response => response.json())
         .then(({articles}) => {
             this.setState({articles})
+        });
+
+        fetch(`${BASE_URL}/topics`)
+        .then(response=>response.json())
+        .then(({topics})=>{
+            const newTopics = topics.map(
+                ({title, _id}) => ({value: _id, text: title})
+            )
+            this.setState({topics:newTopics});
         });
     }
 
@@ -26,6 +38,9 @@ class ArticleList extends React.Component {
         const {articles} = this.state;
         return (
             <div className="ArticleList section">
+                <div>
+                    Display articles on <OrderDropDown entries={this.state.topics}/> by <OrderDropDown entries={this.state.authors}/>
+                </div>
                 <div className="columns is-multiline">
                     {articles.map(
                         article => {
