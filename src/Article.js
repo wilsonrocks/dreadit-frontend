5890c27a66cbe8a45a702984d2f5394c7c2b5d76
James Wilson <jwilsoncalderdale@gmail.com>
2018-07-05 13:23:29 +0100

    merge Article and ArticleList

--- src/Article.js
+++ src/Article.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Avatar from './Avatar';
+import AuthorName from './AuthorName';
 
 import CommentList from './CommentList';
+
 import NotFound from './NotFound';
 import ServerError from './ServerError';
 
@@ -10,56 +12,42 @@ import {BASE_URL} from './constants';
 class Article extends React.Component {
 
     state = {
-      article: null,
-      error: null,
-    };
+        article: {
+            _id: '',
+            title: '',
+            authorName: '',
+            avatarUrl: '',
+            body: '',
+            topicName: '',
+            topicId: '',
+            votes: 0,
+            commentCount: 0,
+        },
+        comments: [],
+    }
 
     componentDidMount () {
         const {_id} = this.props.match.params;
+        console.dir(this.state)
+        
         fetch(`${BASE_URL}/articles/${_id}`)
-            .then(response => {
-                if (response.ok) return response.json();
-                else throw new Error(response.status)
-            })
-            .then(({article}) => {
-                this.setState({article}, () => {
-                    document.title = this.state.article.title;
-                });
-            })
-            .catch(({message}) => {
-                this.setState({error: message});
-            });
-    };
-
-    render () {
-      const {article, error} = this.state;
-      if (error === '500') return <ServerError/>;
-      if (error) return <NotFound/>;
-      return article ? <DisplayArticle {...article}/>:null;
-    };
-  }
-
-class DisplayArticle extends React.Component {
-
-    state = {
-        name: null,
-        topic: null,
-        avatar_url: null,
-        comments: [],
-    };
-
-    componentDidMount () {
-        const {_id, belongs_to} = this.props;
-        const {created_by: {avatar_url, name}} = this.props;
+        .then(response => response.json())
+        .then(({article}) => {
+            const {_id, votes, title, body} = article;
+            const {name: authorName, avatar_url: avatarUrl} = article.created_by;
+            const {title: topicName} = article.belongs_to;
+            this.setState({article:{
+                _id, votes, title, body, authorName, avatarUrl, topicName
+            }})
+            
+        })
+        .then(() => console.dir(this.state));
 
         fetch(`${BASE_URL}/articles/${_id}/comments`)
-        .then(response => {
-            return response.json()}
-        )
+        .then(response => response.json())
         .then(({comments}) => this.setState({comments}));
+    }
 
-        this.setState({avatar_url, name, topicName: belongs_to.title});
-    };
 
     changeVoting = (_id, delta) => {
         const {comments} = this.state;
@@ -71,7 +59,7 @@ class DisplayArticle extends React.Component {
             });
 
         this.setState({comments: newComments});
-    };
+    }
 
     optimisticallyAddComment = (comment) => {
 
@@ -82,24 +70,28 @@ class DisplayArticle extends React.Component {
     }
 
     render () {
-        const {title, body, _id} = this.props
-        const {name, avatar_url, comments, topicName} = this.state;
-
+        const {topicName, title, avatarUrl, authorName,
+            authorId, body, _id} = this.state.article;
+        const {comments} = this.state;
         return (
             <div className="article section">
                 <p className="is-size-3"> {topicName} </p>
                 <h1 className="title">{title}</h1>
-                <h2 className="subtitle"><Avatar avatar_url={avatar_url}/>{name}</h2>
-                <p>{body}</p>
-                <CommentList
-                    comments={comments}
-                    changeVoting={this.changeVoting}
-                    _id={_id}
-                    optimisticallyAddComment={this.optimisticallyAddComment}
-                    />
+                 <h2 className="subtitle">
+                     <Avatar avatar_url={avatarUrl}/>
+                     <AuthorName name={authorName} _id={authorId}/>
+                 </h2>
+                  <p>{body}</p>
+                  <CommentList
+                      comments={comments}
+                      changeVoting={this.changeVoting}
+                      _id={_id}
+                      optimisticallyAddComment={this.optimisticallyAddComment}
+                      />
             </div>
         );
-    };
+    }
+
 }
 
 export default Article;
