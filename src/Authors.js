5ae06b459caa0dff14a72f8b8bde77c09996d0a8
James Wilson <jwilsoncalderdale@gmail.com>
2018-07-05 14:54:02 +0100

    make Component for displaying a list of authors

--- src/Authors.js
+++ src/Authors.js
@@ -1,5 +1,62 @@
 import React from 'react';
 
-const Authors = () => <div>Authors</div>;
+import Avatar from './Avatar.js';
+import AuthorName from './AuthorName.js';
+
+import {BASE_URL} from './constants.js';
+
+class Authors extends React.Component {
+
+  state = {
+    authors: [],
+  }
+
+  componentDidMount () {
+    fetch(`${BASE_URL}/users`)
+    .then(response => response.json())
+    .then(({users}) => this.setState({authors:users}));
+  }
+
+  render () {
+    const {authors} = this.state;
+    return (
+      <div className="section">
+        <table className="table is-hoverable">
+          <tbody>
+            {authors.map(
+              ({name, avatar_url, _id}) => (
+                <tr>
+
+                  <td>
+                    <Avatar
+                      name={name}
+                      avatar_url={avatar_url}
+                      _id={_id}
+                    />
+                  </td>
+
+                  <td>
+                    <AuthorName
+                      name={name}
+                      _id={_id}
+                    />
+                  </td>
+
+                  <td>
+                    {"K"} articles
+                  </td>
+
+                  <td>
+                    {"N"} total votes
+                  </td>
+
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+  }
+}
 
 export default Authors;
