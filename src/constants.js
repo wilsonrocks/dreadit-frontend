718949e5b87e11dc3ee3b04cc733a015b2f4d3b8
James Wilson <jwilsoncalderdale@gmail.com>
2018-07-08 10:17:17 +0100

    make app choose between localhost and hosted API based on environment

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,5 @@
-// export const BASE_URL = 'https://immense-spire-53107.herokuapp.com/api';
-export const BASE_URL = 'http://localhost:3000/api';
+export const BASE_URL = 
+  process.env.NODE_ENV === 'production'
+  ? 'https://immense-spire-53107.herokuapp.com/api'
+  : 'http://localhost:3000/api';
+  
